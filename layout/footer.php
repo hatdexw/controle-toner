01eae96a74aeb76c236a24fc84a3ebe8a5aafc5a
hatdexw <118711265+hatdexw@users.noreply.github.com>
2025-07-31 14:49:53 -0300

    teste

--- layout/footer.php
+++ layout/footer.php
@@ -1,5 +1,5 @@
     </main>
-    <footer class="bg-gray-800 text-white py-4 text-center mt-auto">
+    <footer class="bg-gray-800 text-gray-300 py-4 text-center mt-auto shadow-inner">
         <div class="container mx-auto">
             <p>&copy; <?php echo date('Y'); ?> Controle de Toners. Todos os direitos reservados.</p>
         </div>
